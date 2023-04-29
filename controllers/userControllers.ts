b8db7a0439de5fb0a415db5aa6faa234fe3de79f
manan-eth <abdul.suleman@amalitech.org>
2023-04-29 11:17:54 +0000

    feat: updated the user functions t o asynchronous

--- controllers/userControllers.ts
+++ controllers/userControllers.ts
@@ -1,20 +1,20 @@
 import { Response, Request } from "express";
 
-export const getAllUsers = (req:Request,res:Response)=>{
+export const getAllUsers = async(req:Request,res:Response)=>{
     res.send('get all users')
 }
 
-export const getUser = (req:Request,res:Response)=>{
+export const getUser =async (req:Request,res:Response)=>{
     const {id} = req.params;
     res.send(`get user with id ${id}`)
 }
 
-export const updateUser = (req:Request,res:Response)=>{
+export const updateUser = async(req:Request,res:Response)=>{
     const {id} = req.params;
     res.send(`update user with id ${id}`)
 }
 
-export const deleteUser = (req:Request,res:Response)=>{
+export const deleteUser = async(req:Request,res:Response)=>{
     const {id} = req.params;
     res.send(`Delete user with id ${id}`)
 }
