9796acabdee4a198a80d16665bfa0064e8be9b3d
manan-eth <abdul.suleman@amalitech.org>
2023-05-03 14:43:42 +0000

    fix: Fix type errors with Login

--- controllers/authControllers.ts
+++ controllers/authControllers.ts
@@ -1,61 +1,88 @@
-import { Response,Request} from "express"
-import {hash} from 'bcryptjs';
-import {sign} from 'jsonwebtoken'
+import { Request, Response } from "express";
+import { hash } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 import db from "../db";
 import { SECRET } from "../config";
-
-
-
-interface AuthenticatedRequest extends Request {
-    user?: { id: string; email: string; name:string
-        created_at:Date };
+interface User {
+    name: string;
+    
+    password: string;
   }
-
-export const signUp = async(req:Request,res:Response) => {
-    const {name, email, password} = req.body;
-    try {
-        const hashedPassword = await hash(password,10);
-
-        await db.query('insert into users(name,email,password) values ($1, $2, $3)',[name,email,hashedPassword])
-
-        return res.status(201).json({
-            success:true,
-            message:'Registration Successful'
-        })
-    } catch (error:unknown) {
-        if (error instanceof Error) {
-            return res.status(500).json({
-                error: error.message
-            })
-        }
-    }
+interface UserWithIdAndEmail extends User {
+  id: number;
+  email: string;
 }
 
-export const logIn = async(req:AuthenticatedRequest, res:Response) =>{
-    let user = req.user;
-    if(!user){
-        throw new Error('User not found')
-    }
+export const signUp = async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
+  try {
+    const hashedPassword = await hash(password, 10);
 
-    let payload = {
-        id:user.id,
-        email:user.email
+    await db.query("insert into users(name,email,password) values ($1, $2, $3)", [
+      name,
+      email,
+      hashedPassword,
+    ]);
+
+    return res.status(201).json({
+      success: true,
+      message: "Registration Successful",
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return res.status(500).json({
+        error: error.message,
+      });
     }
+  }
+};
+
+export const logIn = async (req: Request, res: Response) => {
+  let user = req.user as UserWithIdAndEmail;
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  let payload = {
+    id: user.id,
+    email: user.email,
+  };
+
+  try {
+    if (!SECRET) {
+      throw new Error("Secret key is not defined");
+    }
+    const token = sign(payload, SECRET);
+
+    return res
+      .status(200)
+      .cookie("token", token, { httpOnly: true })
+      .json({
+        success: true,
+        message: "Logged In",
+      });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return res.status(500).json({
+        error: error.message,
+      });
+    }
+  }
+};
+
+
+export const logout =(req:Request,res:Response)=>{
     try {
-        if (!SECRET) {
-            throw new Error('Secret key is not defined');
-          }
-        const token = sign(payload, SECRET)
-
-        return res.status(200).cookie('token',token,{httpOnly:true}).json({
+        res.status(200).clearCookie('token',{httpOnly:true}).json({
             success:true,
-            message:'Logged In'
+            message:'Log out succensfully'
         })
-    } catch (error:unknown) {
+    } catch (error: unknown) {
         if (error instanceof Error) {
-            return res.status(500).json({
-                error: error.message
-            })
+          return res.status(500).json({
+            error: error.message,
+          });
         }
-    }
+      }
 }
