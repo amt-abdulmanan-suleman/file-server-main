de8f65bb13b5198666d233f6d87c71227c8a8a31
manan-eth <abdul.suleman@amalitech.org>
2023-05-03 14:46:42 +0000

    Refactor: remove AuthenticatedRequest interface

--- middlewares/validation-middleware.ts
+++ middlewares/validation-middleware.ts
@@ -1,6 +1,11 @@
 import { validationResult } from "express-validator";
 import { Request,Response,NextFunction } from "express";
 
+interface AuthenticatedRequest extends Request {
+    user?: { id: string; email: string; name:string
+        created_at:Date };
+  }
+
 export const validationMiddleware = (req:Request,res:Response,next:NextFunction) =>{
     let err = validationResult(req)
 
