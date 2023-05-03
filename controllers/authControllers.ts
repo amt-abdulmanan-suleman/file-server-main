f867bb6da9d8d31278425b1515d8e418c209f5b5
manan-eth <abdul.suleman@amalitech.org>
2023-05-03 02:15:29 +0000

    feat: define the logic of signup controller func

--- controllers/authControllers.ts
+++ controllers/authControllers.ts
@@ -1,8 +1,30 @@
-import { Response,Request } from "express"
+import { Response,Request} from "express"
+import {hash} from 'bcryptjs'
+import db from "../db";
+
+interface MyError{
+    code:number;
+    message:string
+}
 
 export const signUp = async(req:Request,res:Response) => {
-    const {name,email, phone} = req.body;
-    res.send(`info: ${name},${email},${phone}`)
+    const {name, email, password} = req.body;
+    try {
+        const hashedPassword = await hash(password,10);
+
+        await db.query('insert into users(name,email,password) values ($1, $2, $3)',[name,email,hashedPassword])
+
+        return res.status(201).json({
+            success:true,
+            message:'Registration Successful'
+        })
+    } catch (error:unknown) {
+        if (error instanceof Error) {
+            return res.status(500).json({
+                error: error.message
+            })
+        }
+    }
 }
 
 export const logIn = async(req:Request, res:Response) =>{
