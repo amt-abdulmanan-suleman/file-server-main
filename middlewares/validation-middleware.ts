087c2ae5cd0f7e3c4dd81fa4e6a6d4928924cb3d
manan-eth <abdul.suleman@amalitech.org>
2023-05-03 02:16:53 +0000

    feat: Validation middleware for sign up func

---
+++ middlewares/validation-middleware.ts
@@ -0,0 +1,13 @@
+import { validationResult } from "express-validator";
+import { Request,Response,NextFunction } from "express";
+
+export const validationMiddleware = (req:Request,res:Response,next:NextFunction) =>{
+    let err = validationResult(req)
+
+    if(!err.isEmpty()){
+        return res.status(400).json({
+            errors:err.array()
+        })
+    }
+    next()
+}
