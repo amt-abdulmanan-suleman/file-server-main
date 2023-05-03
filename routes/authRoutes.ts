8981621afadb7b6c9bd8adea64a0a401ab6098e4
manan-eth <abdul.suleman@amalitech.org>
2023-05-03 14:47:32 +0000

    feat: add support for Logout routes

--- routes/authRoutes.ts
+++ routes/authRoutes.ts
@@ -2,13 +2,15 @@ import express from 'express'
 import validator from '../validators/auth'
 
 
-import { signUp,logIn } from '../controllers/authControllers';
+import { signUp,logIn, logout } from '../controllers/authControllers';
 import { validationMiddleware } from '../middlewares/validation-middleware';
+import { userAuth } from '../middlewares/auth-middleware';
 
 
 const router = express.Router();
 router.post('/signup',validator.registerValidation,validationMiddleware,signUp)
 
 router.post('/login',validator.loginValidation,validationMiddleware,logIn)
+router.get('/logout',userAuth,logout)
 
 export default router;
