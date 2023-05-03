bde9e0388593d0ddd489a4c846ba64ccc06301e1
manan-eth <abdul.suleman@amalitech.org>
2023-05-03 13:05:15 +0000

    grab and export the SECRET key from .env

--- config.ts
+++ config.ts
@@ -3,4 +3,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const PORT = process.env.PORT;
-export const PASSWORD = process.env.PASSWORD
+export const PASSWORD = process.env.PASSWORD;
+export const SECRET = process.env.SECRET;
