e6d46495dbe0db94523ae6994debb3fda7ae0199
manan-eth <abdul.suleman@amalitech.org>
2023-05-04 15:13:14 +0000

    feat: import and export email and email password

--- config.ts
+++ config.ts
@@ -5,3 +5,5 @@ dotenv.config();
 export const PORT = process.env.PORT;
 export const PASSWORD = process.env.PASSWORD;
 export const SECRET = process.env.SECRET;
+export const EMAIL_USER = process.env.EMAIL;
+export const EMAIL_PWD = process.env.EMAILPWD
