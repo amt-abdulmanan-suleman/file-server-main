306509ed9d313d80b42a5b9b45d37cccb9f87924
manan-eth <abdul.suleman@amalitech.org>
2023-05-08 00:46:31 +0000

    refactor: remove redundant codes

--- Server/middlewares/validation-middleware.ts
+++ Server/middlewares/validation-middleware.ts
@@ -1,10 +1,7 @@
 import { validationResult } from "express-validator";
 import { Request,Response,NextFunction } from "express";
 
-interface AuthenticatedRequest extends Request {
-    user?: { id: string; email: string; name:string
-        created_at:Date };
-  }
+
 
 export const validationMiddleware = (req:Request,res:Response,next:NextFunction) =>{
     let err = validationResult(req)
