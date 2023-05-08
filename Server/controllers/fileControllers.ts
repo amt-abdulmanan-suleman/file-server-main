04368d14cab62b7640da84a3fa8141312d42724f
manan-eth <abdul.suleman@amalitech.org>
2023-05-08 00:42:53 +0000

    feat: add support for the file routes

--- Server/controllers/fileControllers.ts
+++ Server/controllers/fileControllers.ts
@@ -1,16 +1,86 @@
 import { Response, Request } from "express";
+import { transporter } from "../utils/transporter";
 import db from "../db";
 
 
 
 export const getFiles = async(req:Request,res:Response) =>{
-    res.send('get al files')
+    try {
+        
+        const result = await db.query('SELECT * FROM files');
+        const files = result.rows.map(file => {
+          return {
+            file,
+            url: `http://${req.headers.host}/${file.path}`
+          }
+        });
+        res.json({ files });
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          return res.status(500).json({
+            error: error.message,
+          });
+        }
+    }
 }
 
-export const getFile = async(req:Request,res:Response) =>{
-    res.send(`get a file ${req.params.id}`)
+export const downloadFile = async(req:Request,res:Response) =>{
+    const fileId = req.params.id;
+    const query = 'SELECT name, path FROM files WHERE id = $1';
+    const values = [fileId];
+    try {
+        const result = await db.query(query, values);
+        const file = result.rows[0];
+        const filePath = file.path;
+        res.download(filePath);
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+          return res.status(500).json({
+            error: error.message,
+          });
+        }
+    }
 }
 
+export const sendFile =async (req:Request,res:Response) => {
+    const fileId = req.params.id;
+    const {user_email,receipient_email} = req.body
+    const query = 'SELECT name, path FROM files WHERE id = $1';
+    const values = [fileId];
+
+    try {
+        const result = await db.query(query,values);
+        const file = result.rows[0];
+        const filePath = file.path;
+        const mailOptions = {
+            from: user_email,
+            to: receipient_email,
+            subject: 'File Attachment',
+            attachments: [
+            {
+                filename: file.name,
+                path: filePath,
+            },
+            ],
+        };
+
+        transporter.sendMail(mailOptions, (err, info) => {
+            if (err) {
+            console.error(err);
+            res.status(500).send('Error sending email');
+            } else {
+            console.log(`Email sent: ${info.response}`);
+            res.status(200).send('File sent as email attachment');
+            }
+        });
+    }catch (error: unknown) {
+        if (error instanceof Error) {
+          return res.status(500).json({
+            error: error.message,
+          });
+        }
+    }
+}
 
 export const postFile = async(req:Request,res:Response) =>{
     const file = req.file;
@@ -33,5 +103,19 @@ export const postFile = async(req:Request,res:Response) =>{
     }
 }
 export const deleteFiles = async(req:Request,res:Response) =>{
-    res.send(`Delete a file ${req.params.id}`)
+    const {id} = req.params;
+
+    try{
+      await db.query('delete from files where id=$1',[id]);
+      res.status(200).json({
+        success:true,
+        message:`File with id ${id} deleted`
+      })
+    }catch (error: unknown) {
+        if (error instanceof Error) {
+          return res.status(500).json({
+            error: error.message,
+          });
+        }
+    }
 }
