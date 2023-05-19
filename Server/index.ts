ee6baa3ccfd659f6592a74294f26be7d40bb206f
manan-eth <abdul.suleman@amalitech.org>
2023-05-19 13:29:00 +0000

    update: support for sharing static files to client

--- Server/index.ts
+++ Server/index.ts
@@ -19,7 +19,7 @@ app.use(cors())
 /* Middleware to parse the body from requests*/
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
-
+app.use('/uploads', express.static('uploads'));
 
 app.use(cookieParser())
 
