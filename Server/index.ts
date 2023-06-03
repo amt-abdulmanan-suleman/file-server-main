613ac057abccdccb88eb6b5cea3118d49f60337f
manan-eth <abdul.suleman@amalitech.org>
2023-06-03 11:49:26 +0000

    uoloads

--- Server/index.ts
+++ Server/index.ts
@@ -19,7 +19,7 @@ app.use(cors())
 /* Middleware to parse the body from requests*/
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
-
+app.use(express.static('uploads'))
 app.use('/uploads', express.static('uploads'));
  
 
