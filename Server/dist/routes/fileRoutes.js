2484534acd5224518fdbe44f8d489e5aec5799b1
manan-eth <abdul.suleman@amalitech.org>
2023-06-03 12:05:27 +0000

    files update

--- Server/dist/routes/fileRoutes.js
+++ Server/dist/routes/fileRoutes.js
@@ -8,6 +8,7 @@ const fileControllers_1 = require("../controllers/fileControllers");
 const file_middleware_1 = __importDefault(require("../middlewares/file-middleware"));
 const admin_middleware_1 = require("../middlewares/admin-middleware");
 const auth_middleware_1 = require("../middlewares/auth-middleware");
+const path_1 = __importDefault(require("path"));
 const router = express_1.default.Router();
 router.get('/', auth_middleware_1.userAuth, fileControllers_1.getFiles);
 router.get('/download/:id', auth_middleware_1.userAuth, fileControllers_1.downloadFile);
@@ -15,4 +16,29 @@ router.get('/:id', admin_middleware_1.adminAuth, fileControllers_1.getFile);
 router.post('/send/', auth_middleware_1.userAuth, fileControllers_1.sendFile);
 router.post('/upload', admin_middleware_1.adminAuth, file_middleware_1.default.single('file'), fileControllers_1.postFile);
 router.delete('/:id', admin_middleware_1.adminAuth, fileControllers_1.deleteFiles);
+// Set the correct MIME type for file downloads
+router.use('/uploads', express_1.default.static('uploads', {
+    setHeaders: (res, filePath) => {
+        const ext = path_1.default.extname(filePath);
+        const contentType = getContentType(ext);
+        res.setHeader('Content-Type', contentType);
+    },
+}));
 exports.default = router;
+// Function to determine the MIME type based on file extension
+function getContentType(fileExt) {
+    switch (fileExt) {
+        case '.pdf':
+            return 'application/pdf';
+        case '.jpg':
+        case '.jpeg':
+            return 'image/jpeg';
+        case '.png':
+            return 'image/png';
+        case '.gif':
+            return 'image/gif';
+        // Add more cases for other file types if needed
+        default:
+            return 'application/octet-stream';
+    }
+}
--- Server/routes/fileRoutes.ts
+++ Server/routes/fileRoutes.ts
@@ -1,17 +1,45 @@
 import express from 'express';
-import { deleteFiles, downloadFile, getFile, getFiles, postFile,sendFile } from '../controllers/fileControllers';
+import { deleteFiles, downloadFile, getFile, getFiles, postFile, sendFile } from '../controllers/fileControllers';
 import upload from "../middlewares/file-middleware";
 import { adminAuth } from '../middlewares/admin-middleware';
 import { userAuth } from '../middlewares/auth-middleware';
+import path from 'path';
 
 const router = express.Router();
 
-router.get('/',userAuth, getFiles)
-router.get('/download/:id',userAuth,downloadFile)
-router.get('/:id',adminAuth,getFile)
-router.post('/send/',userAuth,sendFile)
-router.post('/upload',adminAuth,upload.single('file'),postFile)
-router.delete('/:id',adminAuth, deleteFiles)
+router.get('/', userAuth, getFiles);
+router.get('/download/:id', userAuth, downloadFile);
+router.get('/:id', adminAuth, getFile);
+router.post('/send/', userAuth, sendFile);
+router.post('/upload', adminAuth, upload.single('file'), postFile);
+router.delete('/:id', adminAuth, deleteFiles);
 
+// Set the correct MIME type for file downloads
+router.use('/uploads', express.static('uploads', { 
+  setHeaders: (res, filePath) => {
+    const ext = path.extname(filePath);
+    const contentType = getContentType(ext);
+    res.setHeader('Content-Type', contentType);
+  },
+}));
 
-export default router
+export default router;
+
+// Function to determine the MIME type based on file extension
+function getContentType(fileExt:string) {
+  switch (fileExt) {
+    case '.pdf':
+      return 'application/pdf';
+    case '.jpg':
+    case '.jpeg':
+      return 'image/jpeg';
+    case '.png':
+      return 'image/png';
+    case '.gif':
+      return 'image/gif';
+    // Add more cases for other file types if needed
+    default:
+      return 'application/octet-stream';
+  }
+}
+ 
