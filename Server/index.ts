243333222184275161097aeeae868477995cdefd
manan-eth <abdul.suleman@amalitech.org>
2023-06-03 10:56:14 +0000

    files

--- Server/index.ts
+++ Server/index.ts
@@ -20,7 +20,7 @@ app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/dist', express.static('dist'));
+app.use( express.static('dist'));
  
 
 app.use(cookieParser())
