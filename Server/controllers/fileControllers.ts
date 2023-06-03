4c485242baa6b1503370f3ff8c2d9d91f656105c
manan-eth <abdul.suleman@amalitech.org>
2023-06-03 12:37:28 +0000

    uploads

--- Server/controllers/fileControllers.ts
+++ Server/controllers/fileControllers.ts
@@ -9,9 +9,8 @@ export const getFiles = async(req:Request,res:Response) =>{
         
         const result = await db.query('SELECT * FROM files');
         const files = result.rows.map(file => {
-          const host = req.get('host');
-          file.url = `http://${host}/uploads/${file.path}`;
-          return file;
+          file.url = `http://${req.headers.host}/${file.path}`
+          return file
         });
         console.log(files)
         res.json({ files });
